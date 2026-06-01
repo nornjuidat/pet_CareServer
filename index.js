53af79ba2ad175e0691323606b43e4882d2dd893
nornjuidat <njuidatnor750@gmail.com>
2026-06-01 22:49:01 +0300

    added the libery

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 process.env.TZ = "Asia/Jerusalem";
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const port = 6127;
 const app = express();
-app.use(express.json());
+const port = 6127;
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const main_api_R = require("./Routers/main_api_R");
 app.use("/api", main_api_R);
 
-let db_M = require('./db');
+let db_M = require("./db");
 global.db_pool = db_M.pool;
 
 global.GenObj_Mid = require("./Middlewares/GenObj_Mid");
 
-app.use(cors({
-    origin: "http://localhost:5173", // Specify exact origin, not wildcard *
-    credentials: true // Allow credentials
-}));
-
-const path = require('path');
- app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port (http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
 });
